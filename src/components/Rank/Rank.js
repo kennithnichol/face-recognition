07b519cab20cc70def900b2d6d76ae19434e29fc
Kennith Nichol <1321022+kennithnichol@users.noreply.github.com>
2018-10-29 16:16:38 -0700

    Implement Lamda function for rank

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Rank = ({name, entries}) => {
-	return (
-		<div>
-			<div className="white f3">
-				{ `${name}, your current entry count is...`}
+class Rank extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			emoji: ''
+		}
+	}
+
+	componenetDidMount() {
+		this.generateEmoji(this.props.entries)
+	}
+
+	generateEmoji = (entries) => {
+		fetch(`https://g29qd812gg.execute-api.us-west-2.amazonaws.com/prod/rank?rank=${entries}`)
+			.then(response => response.json())
+			.then(data => {
+				this.setState({ emoji: data.input})
+			})
+			.catch(console.log)
+	}
+
+	render() {
+		const {name, entries} = this.props;
+		return (
+			<div>
+				<div className="white f3">
+					{ `${name}, your current entry count is...`}
+				</div>
+				<div className="white f1">
+					{ entries }
+				</div>
 			</div>
-			<div className="white f1">
-				{ entries }
-			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default Rank;

2f5acb9d8a21956e0f86006ed1c7d1c9bad54ed7
Ken Nichol <ken.nichol@live.ca>
2019-07-04 11:12:25 -0700

    Updates

---
+++ precache-manifest.af20024567096c17117ac5b5727b5ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bb3f86a09913a8730ca1365ca4834c",
+    "url": "/face-recgonition/index.html"
+  },
+  {
+    "revision": "15ace377a2405a9f0336",
+    "url": "/face-recgonition/static/css/2.deb68f0c.chunk.css"
+  },
+  {
+    "revision": "fcf442d7ef833a6443b8",
+    "url": "/face-recgonition/static/css/main.d3b8a719.chunk.css"
+  },
+  {
+    "revision": "15ace377a2405a9f0336",
+    "url": "/face-recgonition/static/js/2.a31dcc04.chunk.js"
+  },
+  {
+    "revision": "fcf442d7ef833a6443b8",
+    "url": "/face-recgonition/static/js/main.6d9a7b2e.chunk.js"
+  },
+  {
+    "revision": "2c704fd68342eba4e5bd",
+    "url": "/face-recgonition/static/js/runtime~main.543b0e61.js"
+  }
+]);
